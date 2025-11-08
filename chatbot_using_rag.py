94614a8864f1533fa8ffabb922d9bfaf25af1cac
RiyaRojesara <urvilriyasoni@gmail.com>
2025-11-08 14:56:49 +0530

    Clean upload - no large files

---
+++ chatbot_using_rag.py
@@ -0,0 +1,111 @@
+import streamlit as st
+from langchain_community.document_loaders import PyPDFLoader   
+from langchain_text_splitters.character import CharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
+from langchain_groq import ChatGroq
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from dotenv import load_dotenv
+import os
+
+# Load environment variables
+load_dotenv()
+
+working_dir = os.path.dirname(os.path.abspath(__file__))
+
+# ------------------------------------- INDEXING -----------------------------------------
+
+def load_document(file_path):
+    loader = PyPDFLoader(file_path)   
+    documents = loader.load()
+    return documents
+
+# ------------------------------------- CHUNKING & STORE IN VECTOR STORE -----------------------------------------
+
+def setup_vectorstore(documents):
+    embeddings = HuggingFaceBgeEmbeddings(
+        model_name="BAAI/bge-small-en",
+        model_kwargs={"device": "cpu"},
+        encode_kwargs={"normalize_embeddings": True}
+    )
+
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200
+    )
+    doc_chunks = text_splitter.split_documents(documents)
+    vectorstore = FAISS.from_documents(doc_chunks, embeddings)
+    return vectorstore
+
+# ------------------------------------- CREATE CONVO. CHAIN -----------------------------------------
+
+def create_chain(vectorstore):
+    llm = ChatGroq(
+        model='llama-3.3-70b-versatile',
+        temperature=0.4
+    )
+
+    memory = ConversationBufferMemory(
+        memory_key='chat_history',
+        return_messages=True,
+        output_key='answer'
+    )
+
+    chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory,
+        verbose=True
+    )
+    return chain
+
+# ------------------------------------- STREAMLIT UI -----------------------------------------
+
+st.set_page_config(
+    page_title="Chat with your documents",
+    page_icon="🕵🏻",
+    layout="centered"
+)
+
+st.title("Chat with your documents")
+
+# Initialize chat history
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+# File upload
+uploaded_file = st.file_uploader(label="Upload PDF file", type=['pdf'])
+
+if uploaded_file:
+    file_path = os.path.join(working_dir, uploaded_file.name)
+    with open(file_path, 'wb') as f:
+        f.write(uploaded_file.getbuffer())
+
+    if "vectorstore" not in st.session_state:
+        st.session_state.vectorstore = setup_vectorstore(load_document(file_path))
+
+    if 'conversation_chain' not in st.session_state:
+        st.session_state.conversation_chain = create_chain(st.session_state.vectorstore)
+
+# Show previous messages
+for message in st.session_state.chat_history:
+    with st.chat_message(message['role']):
+        st.markdown(message['content'])
+
+# User input
+user_input = st.chat_input("Ask me something...")
+
+if user_input:
+    st.session_state.chat_history.append({'role': "user", "content": user_input})
+
+    with st.chat_message('user'):
+        st.markdown(user_input)
+
+    with st.chat_message("ai"):
+        response = st.session_state.conversation_chain({'question': user_input})
+        ai_response = response['answer']
+        st.markdown(ai_response)
+
+        st.session_state.chat_history.append({'role': 'ai', "content": ai_response})
